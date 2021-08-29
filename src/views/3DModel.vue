41f4b92f757dfafa596b6a0e8dff92620e66a801
Takenori Kusaka <kokorokagami@gmail.com>
2021-08-29 21:49:34 +0900

    modelupdate

--- src/views/3DModel.vue
+++ src/views/3DModel.vue
@@ -4,7 +4,9 @@
     <v-card-title>
       <h2>3d model</h2>
     </v-card-title>
-    <model-stl src="/na.stl"></model-stl>
+    <!--<model-stl ref="model" src="/na.stl" @on-click="onClick"></model-stl>-->
+    <model-collada src="/na.dae"></model-collada>
+    <model-collada src="/na_freecad.dae"></model-collada>
     <v-card-actions>
     </v-card-actions>
     
@@ -12,11 +14,18 @@
 </template>
 
 <script>
+import { ModelCollada } from 'vue-3d-model'
 import { ModelStl } from 'vue-3d-model'
 
 export default {
     components: {
-        ModelStl
+      ModelCollada,
+      ModelStl
+    },
+    methods: {
+      onClick ($event){
+        console.log($event)
+      }
     }
 }
 </script>
